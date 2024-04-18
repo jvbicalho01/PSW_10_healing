e7c737d87fc7e0a215aaf6d01967b93669d8ed77
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-04-18 16:34:44 -0300

    add agendamento de consultas

--- paciente/urls.py
+++ paciente/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('escolher-horario/<int:id_dados_medicos>/', views.escolher_horario, name='escolher-horario'),
+    path('agendar-horario/<int:id_data_aberta>/', views.agendar_horario, name='agendar-horario'),
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
 
-from medico.models import DadosMedico, Especialidades
+from medico.models import DadosMedico, Especialidades, DatasAbertas
+from .models import Consulta
 
 def home(request):
     if request.method == 'GET':
@@ -19,3 +24,26 @@ def home(request):
 
         return render(request, 'home.html', {'medicos': medicos,
                                              'especialidades': especialidades})
+
+def escolher_horario(request, id_dados_medicos):
+    if request.method == 'GET':
+        medico = DadosMedico.objects.get(id=id_dados_medicos)
+        datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
+        return render(request, 'escolher_horario.html', {'medico': medico,
+                                                         'datas_abertas': datas_abertas})
+
+def agendar_horario(request, id_data_aberta):
+    if request.method == 'GET':
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+        horario_agendado = Consulta(
+            paciente=request.user,
+            data_aberta=data_aberta
+        )
+        horario_agendado.save()
+
+        data_aberta.agendado = True
+        data_aberta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Consulta agendada com sucesso!')
+
+        return redirect('/pacientes/minhas-consultas/')
