cad0c43255a817cbe621d5c7ec788032b8d190b0
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-04-22 14:37:28 -0300

    add detalhes da consulta para o paciente

--- paciente/urls.py
+++ paciente/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('escolher-horario/<int:id_dados_medicos>/', views.escolher_horario, name='escolher-horario'),
     path('agendar-horario/<int:id_data_aberta>/', views.agendar_horario, name='agendar-horario'),
     path('minhas-consultas/', views.minhas_consultas, name='minhas-consultas'),
+    path('consulta/<int:id_consulta>/', views.consulta, name='consulta'),
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -55,3 +55,12 @@ def minhas_consultas(request):
 
     return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas,
                                                      'is_medico': is_medico(request.user)})
+
+def consulta(request, id_consulta):
+    if request.method == 'GET':
+        consulta = Consulta.objects.get(id=id_consulta)
+        dado_medico = DadosMedico.objects.get(user=consulta.data_aberta.user)
+        
+        return render(request, 'consulta.html', {'consulta': consulta,
+                                                 'dado_medico': dado_medico,
+                                                 'is_medico': is_medico(request.user)})
