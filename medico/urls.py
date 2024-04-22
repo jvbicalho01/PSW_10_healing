78f0ae05496b6bcfc8b3f7e388b449c74123c070
Joao Vitor <joaovitorbicalho.dasilva@gmail.com>
2024-04-22 15:46:28 -0300

    add detalhes da consulta para o médico

--- medico/urls.py
+++ medico/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cadastro-medico/', views.cadastro_medico, name='cadastro-medico'),
     path('abrir-horario/', views.abrir_horario, name='abrir-horario'),
     path('consultas-medico/', views.consultas_medico, name='consultas-medico'),
+    path('consulta-area-medico/<int:id_consulta>/', views.consulta_area_medico, name='consulta-area-medico')
 ]
